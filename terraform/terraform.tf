f77eaaf4e55535cbb413703aa1fba318d544fe8e
Isarang <sarang.newaskar@gmail.com>
2024-11-05 09:48:17 +0530

    restore previous version providers

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,38 +1,40 @@
+
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.74.0"
+      version = "~> 5.25.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.6.3"
+      version = "~> 3.5.1"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0.6"
+      version = "~> 4.0.4"
     }
 
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "~> 2.3.5"
+      version = "~> 2.3.2"
     }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.33.0"
+      version = "~> 2.23.0"
     }
   }
 
   backend "s3" {
-    bucket = "gitops1985"
+    bucket = "gitopsterrastate"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
 
   required_version = "~> 1.6.3"
 }
+##
 ##
 ##
